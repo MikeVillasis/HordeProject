2460dfb00e87f04444d4851877df77be2f14df71
Mike <miguelvillasism@gmail.com>
2020-12-21 13:20:36 -0800

    Fixed Directories, added Base Mode

--- Source/HordeProject/Private/Player/Horde_PlayerCharacter.cpp
+++ Source/HordeProject/Private/Player/Horde_PlayerCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Horde_PlayerCharacter.h"
+#include "Player/Horde_PlayerCharacter.h"
 
 // Sets default values
 AHorde_PlayerCharacter::AHorde_PlayerCharacter()
