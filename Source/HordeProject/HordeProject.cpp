d07b964aebd12cc80768700663acda02230ac4ef
Mike <miguelvillasism@gmail.com>
2020-12-21 13:01:00 -0800

    Init commit after git ignore

---
+++ Source/HordeProject/HordeProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HordeProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HordeProject, "HordeProject" );
